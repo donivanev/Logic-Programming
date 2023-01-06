569641a4e8823c15f097e28bfaa77a49ba83000f
Doni <dok246@gmail.com>
2023-01-06 10:50:17 +0200

    Update basicAlgorithms.pl

--- basicAlgorithms.pl
+++ basicAlgorithms.pl
@@ -1,8 +1,19 @@
+% Generate natural numbers
+
 naturals(0).
 naturals(X) :- naturals(Y), X is Y + 1.
 
+% Generate integers
+
+integer(0, 0).
+integer(X, Y) :- X > 0, (Y is X; Y is -X).
+
+% Generate numbers in interval [X, Y].
+
 between_(X, Y, X) :- X <= Y.
 between_(X, Y, Z) :- X < Y, X1 is X + 1, between_(X1, Y, Z).
+
+
 
 is_not_prime(X) :- X1 is X - 1, between_(2, X1), X mod Y =:= 0.
 all_primes(X) :- naturals(X), X > 1, not(is_not_prime(X)).
@@ -10,9 +21,10 @@ all_primes(X) :- naturals(X), X > 1, not(is_not_prime(X)).
 factorial(0, 1).
 factorial(N, X) :- M is N - 1, factorial(M, Y), X is N * Y.
 
+% GCD of two numbers
+
 gcd(A, 0, A).
-gcd(A, B, C) :- B > 0, A >= B, A1 is A - B, gcd(A1, B, C).
-gcd(A, B, C) :- A < B, gcd(B, A, C).
+gcd(A, B, C) :- B > 0, G is A mod B, gcd(B, G, C).
 
 divisor(A, B, C) :- A div C =:= 0, B div C =:= 0.
 common_divisors(A, B, C) :- bet2(1, A, C), divisor(A, B, C).
