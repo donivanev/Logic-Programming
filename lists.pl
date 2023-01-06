3af28c93a4b969d784eace5c407b5e29e8dfef10
Doni <dok246@gmail.com>
2023-01-06 12:37:45 +0200

    Update lists.pl

--- lists.pl
+++ lists.pl
@@ -1,33 +1,33 @@
-%Is list
+% Is list
 
 list_is_list([]).
 list_is_list([_|_]).
 
-%First element
+% First element
 
 list_first(X, [X|_]).
 
-%Last element
+% Last element
 
 list_last(X, [X]).
 list_last(X, [_|T]) :- last(X, T).
 
-%Element at position
+% Element at position
 
 list_element_at(X, [X|_], 1).
 list_element_at(X, [_|T], K) :- K > 1, K1 is K - 1, list_element_at(X, T, K1).
 
-%Is member
+% Is member
 
 list_member(X, [X|_]).
 list_member(X, [_|T]) :- list_member(X, T).
 
-%Length
+% Length
 
 list_length([], 0).
 list_length([_|T], N) :- list_length(T, N1), N is N1 + 1.
 
-%Concatenation
+% Concatenation
 
 list_concat([], L, L).
 list_concat([X|T], L, [X|T2]) :- list_concat(T, L, T2).
@@ -35,7 +35,7 @@ list_concat([X|T], L, [X|T2]) :- list_concat(T, L, T2).
 %list_append(A, T, T) :- list_member(A, T), !.
 %list_append(A, T, [A|T]).
 
-%Delete
+% Delete
 
 list_delete(X, [X], []).
 list_delete(X, [X|L1], L1).
@@ -44,48 +44,48 @@ list_delete(X, [Y|L2], [Y|L1]) :- list_delete(X, L2, L1).
 %list_delete(X, [X|Xs], Xs).
 %list_delete(X, [Y|Ys]) :- list_delete(X, Ys, Zs).
 
-%Insert
+% Insert
 
 list_insert(X, L, R) :- list_delete(X, R, L).
 
-%Reverse
+% Reverse
 
 list_rev([], []).
 list_rev([H|T], Reversed) :- list_rev(T, RevT), list_concat(RevT, [H], Reversed).
 
-%Is permutation
+% Is permutation
 
 list_perm([], []).
 list_perm(L, [X|P]) :- list_delete(X, L, L1), list_perm(L1, P).
 
-%Shift
+% Shift
 
 list_shift([H|T], Shifted) :- list_concat(T, [H], Shifted).
 
-%Order
+% Order
 
 list_order([X, Y|T]) :- X =< Y, list_order([Y|T]).
 list_order([_]).
 
-%Subset
+% Subset
 
 list_subset([], []).
 list_subset([H|T], [H|Subset]) :- list_subset(T, Subset).
 list_subset([_|T], Subset) :- list_subset(T, Subset).
 
-%Union
+% Union
 
 list_union([X|Y], Z, W) :- list_member(X, Z), list_union(Y, Z, W).
 list_union([X|Y], Z, [X|W]) :- \+ list_member(X, Z), list_union(Y, Z, W).
 list_union([], Z, Z).
 
-%Intersection
+% Intersection
 
 list_intersect([X|Y], Z, [X|W]) :- list_member(X, Z), list_intersect(Y, Z, W).
 list_intersect([X|Y], Z, W) :- \+ list_member(X, Z), list_intersect(Y, Z, W).
 list_intersect([], _, []).
 
-%Even / Odd Length
+% Even / Odd Length
 
 list_even_len([]).
 list_even_len([_|T]) :- list_odd_len(T).
@@ -93,13 +93,13 @@ list_even_len([_|T]) :- list_odd_len(T).
 list_odd_len([_]).
 list_odd_len([_|T]) :- list_even_len(T).
 
-%Divide
+% Divide
 
 list_divide([], [], []).
 list_divide([X], [X], []).
 list_divide([X, Y|T], [X|L1], [Y|L2]) :- list_divide(T, L1, L2).
 
-%Min
+% Min
 
 min_(X, Y, X) :- X < Y.
 min_(X, Y, Y) :- X >= Y.
@@ -107,7 +107,7 @@ min_(X, Y, Y) :- X >= Y.
 list_min(X, [X]).
 list_min(X, [H|T]) :- list_min(N, T), min_(H, N, X).
 
-%Max 
+% Max 
 
 max_(X, Y, X) :- X >= Y.
 max_(X, Y, Y) :- X < Y.
@@ -115,19 +115,19 @@ max_(X, Y, Y) :- X < Y.
 list_max([X], X).
 list_max([X, Y|Rest], Max) :- list_max([Y|Rest], MaxRest), max_(X, MaxRest, Max).
 
-%Sum
+% Sum
 
 list_sum([], 0).
 list_sum([H|T], Sum) :- list_sum(T, SumTemp), Sum is H + SumTemp.
 
-%Is Preffix
+% Is Preffix
 
 list_preffix(P, L) :- list_append(P, _, L).
 %OR
 %list_preffix([], L).
 %list_preffix([X|P], [X|L]) :- list_prefix(P, L).
 
-%Is Suffix
+% Is Suffix
 
 list_suffix(S, L) :- list_append(_, S, L).
 
@@ -138,11 +138,17 @@ in_difference(X, A, B) :- member(X, A), \+ member(X, B).
 is_subset_of(A, B) :- \+((member(X, A), \+(member(X, B)))).
 are_equal(A, B) :- is_subset_of(A, B), is_subset_of(B, A).
 
-%Is Sublist
+% Is Sublist
 
 list_sublist(S, L) :- list_append(X, _, L), list_append(_, S, X).
 
-%Is sorted
+% Split
+
+list_split([], [], []).
+list_split([H|T], [H|L], R) :- list_split(T, L, R).
+list_split([H|T], L, [H|R]) :- list_split(T, L, R).
+
+% Is sorted
 
 list_sorted([]).
 list_sorted([_]).
@@ -150,11 +156,11 @@ list_sorted([X, Y|L]) :- X < Y, list_sorted([Y|L]).
 %OR
 %is_sorted(L) :- not((append(_, [A, B|_], L), A > B)).
 
-%Simplest sort
+% Simplest sort
 
 simplestSort(L, SL) :- permutation(L, SL), is_sorted(SL).
 
-%Merge sort
+% Merge sort
 
 split([], [], []).
 split([X], [X], []).
