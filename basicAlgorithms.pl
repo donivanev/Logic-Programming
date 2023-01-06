c635c5f35918a47752070d87255e84cd2472c6d2
Doni <dok246@gmail.com>
2023-01-06 10:29:22 +0200

    Update basicAlgorithms.pl

--- basicAlgorithms.pl
+++ basicAlgorithms.pl
@@ -11,11 +11,11 @@ factorial(0, 1).
 factorial(N, X) :- M is N - 1, factorial(M, Y), X is N * Y.
 
 gcd(A, 0, A).
-gcd(A, B, D) :- B > 0, A >= B, A1 is A - B, gcd(A1, B, D).
-gcd(A, B, D) :- A < B, gcd(B, A, D).
+gcd(A, B, C) :- B > 0, A >= B, A1 is A - B, gcd(A1, B, C).
+gcd(A, B, C) :- A < B, gcd(B, A, C).
 
-divisor(A, B, D) :- A div D =:= 0, B div D =:= 0.
-common_divisors(A, B, D) :- bet2(1, A, D), divisor(A, B, D).
+divisor(A, B, C) :- A div C =:= 0, B div C =:= 0.
+common_divisors(A, B, C) :- bet2(1, A, C), divisor(A, B, C).
 
-not_greatest_common_div(A, B, D) :- common_divisors(A, B, D), D < D1.
-greatest_common_div(A, B, D) :- common_divisors(A, B, D), not(not_greatest_common_div(A, B, D)).
+not_greatest_common_div(A, B, C) :- common_divisors(A, B, C), C < C1.
+greatest_common_div(A, B, C) :- common_divisors(A, B, C), not(not_greatest_common_div(A, B, C)).
